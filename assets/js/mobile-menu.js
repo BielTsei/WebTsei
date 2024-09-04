de25942ea3420b323f8ff847e3c8bcd5477fffed
Gabriel Tsei <92765565+BielTsei@users.noreply.github.com>
2024-09-04 13:47:51 -0300

    Comprenssado css e Js

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,17 +1,1 @@
-const btnMobile = document.getElementById("btn-mobile");
-
-function toogleMenu(event) {
-  if (event.type === "touchstart") event.preventDefault();
-  const nav = document.getElementById("nav");
-  nav.classList.toggle("active");
-  const active = nav.classList.contains("active");
-  event.currentTarget.setAttribute("aria-expanded", active);
-  if (active) {
-    event.currentTarget.setAttribute("aria-label", "Fechar menu");
-  } else {
-    event.currentTarget.setAttribute("aria-label", "Abrir menu");
-  }
-}
-
-btnMobile.addEventListener("click", toogleMenu);
-btnMobile.addEventListener("touchstart", toogleMenu);
+const btnMobile=document.getElementById("btn-mobile");function toogleMenu(t){"touchstart"===t.type&&t.preventDefault();const e=document.getElementById("nav");e.classList.toggle("active");var n=e.classList.contains("active");t.currentTarget.setAttribute("aria-expanded",n),n?t.currentTarget.setAttribute("aria-label","Fechar menu"):t.currentTarget.setAttribute("aria-label","Abrir menu")}btnMobile.addEventListener("click",toogleMenu),btnMobile.addEventListener("touchstart",toogleMenu);
